990a8af4e462c6d4aef6d529fc24ec79c2fa3454
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-22 16:23:11 +0530

    veriable initilize and decleare
    veriable initilize and decleare

---
+++ 01_veriable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Suyog";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Suyog"; // Variable update
+console.log(firstName);
+ 
+ 
+var lastName = "Ghadage"; // variable declaration and initialization
+console.log(lastName);
+ 
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
---
+++ 01_veriableAssign.js
@@ -0,0 +1,29 @@
+var myFullName;
+myFullName = "Suyog Ghadage";
+console.log(myFullName);
+myFullName = "Suyog Ganpat Ghadage";
+console.log(myFullName);
+
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+var firstName = "Suyog";
+console.log("firstName:",  firstName);
+
+var lastName = "Ghadage";
+console.log("lastName:",lastName);
+
+var age = "25";
+console.log("age:",age);
+
+var address = "Gurudhwara chowk Akurdi,pune";
+console.log("address:",address);
+
+var pincode = "411035";
+console.log("pinCode:",pincode);
+
+var gender = "male";
+console.log("gender:",gender);
+
+var isMarried = "Single";
+console.log("isMarried:",isMarried);
