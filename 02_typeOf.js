4244c15b8fa9e97ff68c9f165369f6f93f64e13b
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-23 14:32:00 +0530

    typeOf
    typeOf

---
+++ 02_typeOf.js
@@ -0,0 +1,21 @@
+console.log("--------Before initialization------");
+var bankName;
+console.log(bankName);
+console.log("--------After initialization--------");
+bankName = "SBI Bank";
+console.log(bankName,typeof bankName);
+
+
+var marks = '90%';
+console.log("marks",marks,"--type--",typeof marks);
+
+
+var isWorking;
+console.log(isWorking);
+isWorking = "true";
+console.log("isWorking",isWorking,"--type--",typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("totalCount",totalCount,"--type--",typeof totalCount);
+totalCount = "107";
+console.log("totalCount",totalCount,"--type--",typeof totalCount);
