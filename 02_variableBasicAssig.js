45b6000ff3ee49aff4b857bfd2d9068dd8621848
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-23 12:50:18 +0530

    Variable Basic Assignment
    Variable Basic Assignment

---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,26 @@
+var name = "Suyog";
+console.log("name:",name,"----type---",typeof name);
+
+var gender = "male";
+console.log("gender",gender,"---type---",typeof gender);
+
+var city = "pune";
+console.log("city",city,"---type---",typeof city);
+
+var four = 4;
+console.log("four:",four,"---type---",typeof(four));
+
+var five = 5;
+console.log("five:",five,"---type---",typeof five);
+
+var six = 6;
+console.log("six:",six,"---type---",typeof(six));
+
+var seven = true;
+console.log("seven",seven,"---type---",typeof(seven));
+
+var eight = false;
+console.log("eight",eight,"---type---",typeof eight);
+
+var nine = true;
+console.log("nine:",nine,"---type---",typeof(nine));
