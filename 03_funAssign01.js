1bfed387c0e1dac15bfb203ac47e42ae5cdba75a
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-25 18:28:13 +0530

    funAssign02
    funAssign02

--- 03_funAssign01.js
+++ 03_funAssign01.js
@@ -1,76 +1,82 @@
 console.log("-------Function with no argument and no return type------------");
-function education (){
-    console.log("collage Name : D.Y.Patil collage of engineering,Akurdi");
-    console.log("department : Production Engineering(sandwich)");
-    console.log("Passout year : 2022");
+function education() {
+  console.log("collage Name : D.Y.Patil collage of engineering,Akurdi");
+  console.log("department : Production Engineering(sandwich)");
+  console.log("Passout year : 2022");
 }
 
-function Skills(){
-    console.log("Courses completed : C,C++,data structure,Networking,HTML,CSS,GIT & GITHUb");
-    console.log("courses in live : javaScript,bootstrap,typeScript,Angular");
- 
+function Skills() {
+  console.log(
+    "Courses completed : C,C++,data structure,Networking,HTML,CSS,GIT & GITHUb"
+  );
+  console.log("courses in live : javaScript,bootstrap,typeScript,Angular");
 }
 
-
-
-function PersonalDetail(fristName,lastName){
-    console.log("first Name :",fristName);
-    console.log("last Name :",lastName);
-    console.log("collage name :DYPCOE");
+function PersonalDetail(fristName, lastName) {
+  console.log("first Name :", fristName);
+  console.log("last Name :", lastName);
+  console.log("collage name :DYPCOE");
 }
 
-
-
-function swapValue(val1,val2){
-    var num1 = val1;
-    var num2 = val2;
-    console.log("before Swapping :",num1,num2);
-    var temp =num1;
-    num1 = num2;
-    num2 = temp;
-    console.log("after swapping :",num1,num2);
+function swapValue(val1, val2) {
+  var num1 = val1;
+  var num2 = val2;
+  console.log("before Swapping :", num1, num2);
+  var temp = num1;
+  num1 = num2;
+  num2 = temp;
+  console.log("after swapping :", num1, num2);
 }
 
-
-
-function addThreeValues(value1,value2,value3){
-    var w1=value1;
-    var w2=value2;
-    var w3=value3;
-    var add = w1+w2+w3;
-    console.log("addition of Three is:",add);
-
+function addThreeValues(value1, value2, value3) {
+  var w1 = value1;
+  var w2 = value2;
+  var w3 = value3;
+  var add = w1 + w2 + w3;
+  console.log("addition of Three is:", add);
 }
 
-console.log("*************************************************************************************");
+console.log(
+  "*************************************************************************************"
+);
 
 education();
-console.log("*************************************************************************************");
-
+console.log(
+  "*************************************************************************************"
+);
 
 Skills();
 
-console.log("*************************************************************************************");
+console.log(
+  "*************************************************************************************"
+);
 
+PersonalDetail("Suyog", "Ghadage");
 
-PersonalDetail("Suyog","Ghadage");
+console.log(
+  "*************************************************************************************"
+);
 
-console.log("*************************************************************************************");
+swapValue("virat", "Anushka");
+console.log(
+  "*************************************************************************************"
+);
 
+swapValue("1000", "2000");
 
-swapValue("virat","Anushka");
-console.log("*************************************************************************************");
+console.log(
+  "*************************************************************************************"
+);
 
+addThreeValues("10.23", "600", "40");
+console.log(
+  "*************************************************************************************"
+);
 
-swapValue("1000","2000");
-
-console.log("*************************************************************************************");
-
-
-addThreeValues("10.23","600","40");
-console.log("*************************************************************************************");
-
-
-console.log("*************************************************************************************");
-addThreeValues("Hello","Good","Morning");
-console.log("*************************************************************************************");
+console.log(
+  "*************************************************************************************"
+);
+addThreeValues("Hello", "Good", "Morning");
+console.log(
+  "*************************************************************************************"
+);
