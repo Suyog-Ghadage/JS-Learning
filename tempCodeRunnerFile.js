196b8b2f3b37e03cd78e7c5bdc6fdd85789d1006
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-25 16:42:01 +0530

    function Assignment
    function Assignment

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-var four = 4;
-console.log("four:",four,"---type---",typeof(four));
-
-var five = 5;
-console.log("five:",five,"---type---",typeof five);
-
-var six = 6;
-console.log("six:",six,"---type---",typeof(six));
+10.23
