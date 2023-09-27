39cd688c535cb6724b8b01a179705a7b1728422f
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-27 12:07:39 +0530

    string Basic Assignment
    string Basic Assignment

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,15 @@
+function stringBasics(){
+    console.log("My Dream company is:","Google");
+    var hobby1 = "traveling";
+    var hobby2 = "treaking";
+    var hobby3 = "bikeRiding";
+    console.log("hobby1:",hobby1,"hobby2:",hobby2,"hobby3:",hobby3);
+    // var result = hobby1.concat(hobby2).concat(hobby3);
+    // console.log(result);
+    console.log("number of characters in hobby1:",hobby1.length);
+    console.log("number of characters in hobby2:",hobby2.length);
+    console.log("number of characters in hobby3:",hobby3.length);
+    console.log("sum of the total numbers in all 3 hobbies is:",hobby1.length+hobby2.length+hobby3.length¬);
+    
+}
+stringBasics();
