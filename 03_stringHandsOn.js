a4edc76aa38c55e3e4a34bb778f33e82d2c16f14
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-09-27 13:15:58 +0530

    string Basic
    string Basic

---
+++ 03_stringHandsOn.js
@@ -0,0 +1,11 @@
+
+
+function stringHandsOn(){
+    var string = "   Hey you are doing good,keep it up    ";
+    console.log(`Given String Is : ${string}`);
+    var lengthOfString = string.length;
+    console.log(lengthOfString);
+   
+}
+
+stringHandsOn();
