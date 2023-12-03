09bb6e2cfe9e1695545dea6bb086625ff9f862e5
Suyog Ghadage <126576436+Suyog-Ghadage@users.noreply.github.com>
2023-12-03 18:20:23 +0530

    set and json
    set and json

---
+++ 06_primenumber.js
@@ -0,0 +1,30 @@
+// const num = 7;
+// function isPrimeNumber(num){
+//     for (let index = 2; index < num; index++) {
+//         if (num%index==0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// const result = isPrimeNumber(11);
+// console.log(`Is Number Prime: ${result}`);
+
+
+const array = [11, 3, 6, 10, 43, 28, 50];
+
+
+count = 0;
+    for (let index = 0; index < array.length ; index++) {
+        if (array[index]%2==0) {
+           
+            
+        } else {
+            count++;
+            
+            
+        }
+    }
+    console.log(count);
+
+
---
+++ 10_jsonAssign.js
@@ -0,0 +1,16 @@
+
+    `"name": "Aleix Melon",
+    "id" : "E00245",
+    "role": ["Dev","DBA"],
+    "age":23,
+    "doj":"11-12-2019",
+    "married": false ,
+    "address":{
+        "street" :"32,Lahan st.",
+        "city":"Innsbruck",
+        "country":"Austria",
+
+    },
+    "referred-by ": "E0012",
+`
+
